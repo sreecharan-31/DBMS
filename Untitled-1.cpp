a0e61ff920c83d6c648dc23f9010c2d72bb1c9a2
Sree Charan Reddy <s.sreecharan.31@outllook.com>
2018-05-16 12:37:00 +0530

    Added Add function

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
+  int a,b;
+  cin>>a>>b;
+  int sum=add(a,b);
   cout<<"Nothing"<<endl;
   return(0);
+}
+
+int add(int a, int b)
+{
+    int sum =a+b;
+  return(sum);
 }
